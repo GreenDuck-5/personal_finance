e5a38d77cad11cc3865f0c5df6bcb903cfba993c
Stubbed-My-Toe <isaac.covington@ucas-edu.net>
2026-03-26 09:50:24 -0600

    yummers

---
+++ src/helper.py
@@ -0,0 +1,7 @@
+#isaac stuff here
+import time
+def find_timestamp():
+    #When money comes in, run it throught this function to find it's timestamp,and then write it to money.csv
+    curr = time.ctime(time.time())
+    print("Current time:", curr)
+################################
