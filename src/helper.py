bebf0ed3f8cde8488b74c16cfb2bf75f08afdcb4
Stubbed-My-Toe <isaac.covington@ucas-edu.net>
2026-04-01 09:15:46 -0600

    finishing up more

--- src/helper.py
+++ src/helper.py
@@ -4,6 +4,7 @@
     #When money comes in, run it throught this function to find it's timestamp, and then write it to money.csv
     #curr = time.ctime(time.time())
     #print("Current time:", curr)
-    #whatever the thing coming in will be called, just say timestamp
+    #whatever the thing coming in will be called, just say timestamp.
+    #
 
 #when spending and income data comes in or goes out, take amount, date, and source, a+ to money.csv.
