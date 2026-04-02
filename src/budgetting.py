61bfe36ab52a6a63bc4eff37bd2cd0cecdcb5bf5
GreenDuck-5 <dirk.jones@ucas-edu.net>
2026-04-02 09:25:55 -0600

    budgetting pseudocode

--- src/budgetting.py
+++ src/budgetting.py
@@ -1,7 +1,14 @@
 #DJ, 1st, Budgetting Program
 
 
+# add budget function
 
-    # ask user to add budgetting categories
 
-    # ask user for the budget in that category
+    # ask user to add budgetting categories or quit or edit income
+    
+    # if add budgetting category
+        # ask user for the budget in that category
+        # add category to csv
+
+    # if edit income
+        # let user change what the income is
