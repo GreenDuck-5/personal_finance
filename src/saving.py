7b6aba795fcc9903a6713f4be046f455cd3f2003
GreenDuck-5 <dirk.jones@ucas-edu.net>
2026-04-07 09:51:59 -0600

    saving

--- src/saving.py
+++ src/saving.py
@@ -1,23 +1,54 @@
 # DJ, 1st, Saving Program
 
+from datetime import datetime
+import time as t
 
 # create saving plan function:
 def create_saving_plan():
 
     # ask user what amount theyre saving to
     while True:
-        choice = input("What amount are you saving to?\n")
+        saving_amount = input("What amount are you saving to?\n")
         
-        if choice.isdigit():
+        if saving_amount.isdigit():
             break
         else:
             print("Please enter valid input")
+            continue
+    
     
     # ask user how much money theyre putting in and how often
-    
+    while True:
+        deposit_often = input("How often are you going to put money in? (You'll decide how much you'll deposit next)\n1.) Daily\n2.) Weekly\n3.) Monthly\n")
+        
+        if deposit_often == "1":
+            deposit_often = "daily"
+            break
+        elif deposit_often == "2":
+            deposit_often = "weekly"
+            break
+        elif deposit_often == "3":
+            deposit_often = "monthly"
+            break
+        else:
+            print("Please enter valid input")
+            continue
+
+    while True:
+        deposit_amount = input(f"How much money are you going to put in every {deposit_often}?\n")
+        if deposit_amount.isdigit():
+            break
+        else:
+            print("Please enter valid input")
+            continue
 
     # find monthly expense and add that as a budgetting category
-
+    if deposit_often == "daily":
+        monthly_expense = (deposit_amount * 30) / saving_amount
+    elif deposit_often == "weekly":
+        monthly_expense = (deposit_amount * 4) / saving_amount
+    elif deposit_often == "monthly":
+        monthly_expense = deposit_often / saving_amount
 
 
 # view saving plan function:
